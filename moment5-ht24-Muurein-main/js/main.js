aab0b916c66f33218615f4396015582778920336
Muurein <amanda.l.thim@gmail.com>
2024-10-28 16:06:29 +0100

    build outline, fetch url

--- moment5-ht24-Muurein-main/js/main.js
+++ moment5-ht24-Muurein-main/js/main.js
@@ -17,11 +17,42 @@ document.getElementById("shownumrows").style.display = "none"; // Radera denna r
             1ac. lägg till under mainnavlist.appendChild
             1ad. lägg till under mainnav.appendChild?????
         1b. förs muspekaren över ska (("hover", function)???) ska en info-rutan dyka upp med info om kanalen. Ska läsas in från Sveriges Radois rest-webbtjäsnt - 2.01 i videon
-    2. klickar man på en kanal ska man se en tablå över kanalens program som inte redan har gått (bara dagens fram till midnatt). Följande info om programmet ska finnas med:
+    2. klickar man på en kanal ska man se en tablå över kanalens program som inte redan har gått (bara dagens fram till midnatt - date-objekt, getDate, getHour(?)). Följande info om programmet ska finnas med:
         2a. titel
         2b. undertitel (om det finns), ska annars inte visas och *ska inte dyka upp ett felmeddelande*
         2c. sändnigstid
         2d. info om programmet
             
 */
+
+//1a when the page loads
+window.onload = init;
+
+//1a - gets and shows a list of the channels when the page loads 
+function init() {
+    getChannels();
+}
+
+//1aa + 1ab - get channels to be able to show them in the list
+function getChannels() {
+    //stores the url, changes the recieved data to JSON - GETS ALL CHANNELS???
+    const url = "http://api.sr.se/api/v2/channels?format=json";
+
+    //fetch - calls for the url
+    fetch(url)
+    //turn the response into JSON
+    .then(response => response.json())
+    //från Malins övning - vill få ut värdet ur nycklarna, dock var det variabler så det kanske inte funkar
+    //.then(data => writeChannels(Object.keys(data)(data.channels)))
+    .then(data => console.log(data))
+    //in case something goes wrong
+    .catch(error => console.log("There was an error " + error))
+}
+
+
+
+
+//1ac + 1ad - write out the channels under <ul id="mainnavlist">
+
+
 
