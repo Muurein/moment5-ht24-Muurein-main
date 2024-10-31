c8aa3433f330deea6e37bb283801895f2ddd6304
Muurein <amanda.l.thim@gmail.com>
2024-10-31 12:28:01 +0100

    updated show channels-code, added "hover-box"

--- moment5-ht24-Muurein-main/js/main.js
+++ moment5-ht24-Muurein-main/js/main.js
@@ -31,59 +31,119 @@ document.getElementById("shownumrows").style.display = "none"; // Radera denna r
 //1a when the page loads
 window.onload = init;
 
+
 //1a - gets and shows a list of the channels when the page loads 
 function init() {
     getChannels();
+   // showInfo();
+    getTVTable();
 }
 
 //1aa + 1ab - get channels to be able to show them in the list
 function getChannels() {
-    //stores the url, changes the recieved data to JSON - GETS ALL CHANNELS???
+    //stores the url, changes the recieved data to JSON
     const url = "http://api.sr.se/api/v2/channels?format=json";
 
     //fetch - calls for the url
     fetch(url)
-    //turn the response into JSON
-    .then(response => response.json())
-    //från Malins övning - vill få ut värdet ur nycklarna, dock var det variabler så det kanske inte funkar - VALUES ÄR FEL
-    //.then(data => writeChannels(Object.keys(data.channels))) //KAN OCKSÅ TESTA BARA SKRIVA .then(data => {writeChannels.data}), får inga siffror bara [object object]
-    .then(data => writeChannels(data.channels))
-    .catch(error => console.log("There was an error " + error))
-}
+        //turn the response into JSON
+        .then(response => response.json())
+        //gets the data
+        .then(data => { 
+            console.log(data)
+            writeChannels(data.channels)})
+        .catch(error => console.log("There was an error " + error))
+    }
 
-
-
-//i det här stadiet är arrayen uttagen ur datan. Nu ska vi bara få fram namnen
-//1ac + 1ad + 1b? - write out the channels under <ul id="mainnavlist">
+//1ac + 1ad + 1b
 function writeChannels(channels) {
 
     //get the HTML-element where I want to write out the channels
     const channelsEl = document.getElementById("mainnavlist");
-    console.log(channels);
-    
-  
-//Eftersom det är en array - måste jagf först definiera index och sen namn????????
+   // console.log(channels);
+
     //loops through the given array to create the list of channels
     channels.forEach(channel => {
-        //got help from youtuber ByteGrad at https://www.youtube.com/watch?v=zUcc4vW-jsI
-        //creates a li-element and puts the name-property from the channels-array into the ul-element
-        const markup = `<li>${channel.name}</li>`;
-        document.querySelector("ul").insertAdjacentHTML("beforeend", markup);
+
+         //creates the list item and adds the "hovering-box"
+        let channelLiEl = document.createElement("li");
+        let listTextEl = document.createTextNode(channel.name);
+        channelLiEl.setAttribute("title", channel.tagline);
+        channelLiEl.value = channel;
+
+
+        //smush them together
+        channelLiEl.appendChild(listTextEl);
+        channelsEl.appendChild(channelLiEl);
+        
     })
     
 }
 
+
 //pre-2 - get the channel programs
 function getTVTable() {
+    let url = "http://api.sr.se/api/v2/scheduledepisodes/rightnow?format=json"; 
 
+    console.log(url);
+
+    //fetch - calls for the url
+    fetch(url)
+        //turn the response into JSON
+        .then(response => response.json())
+        //från Malins övning - vill få ut värdet ur nycklarna, dock var det variabler så det kanske inte funkar - VALUES ÄR FEL
+        //.then(data => writeChannels(Object.keys(data.channels))) //KAN OCKSÅ TESTA BARA SKRIVA .then(data => writeChannels.data), får inga siffror bara [object object] .then(data => console.log(data.channels))
+        .then(data => writeTVTable(data.channels))
+        .catch(error => console.log("There was an error " + error))
 }
+
 
 
 
 //2aa + 2ab + 2ac + 2ad write out the channel programs
 function writeTVTable() {
+    //console.table(channels.name.title.description);
+    let tableEl = document.getElementById("info");
+
+    //console.log(tableEl);
+
+    //skapa datum??
+    
 
 }
 
 
 
+
+/*
+        //creates the list item
+       // let channelLiEl = document.createElement("li");
+
+
+
+
+       /försök från föreläsning
+       //add text(value)
+       let textNode = document.createTextNode(channels.name); //BLIR BARA UNDEFINED VA FAN DÅ FÖR
+       channelLiEl.appendChild(textNode);
+       channelsEl.appendChild(channelLiEl);/
+
+       //eget försök som inte funkade
+        itemEl.classname = "item";
+
+        document.getElementsByClassName("item").innerHTML = channel.name;
+        console.log(channel.name);/
+
+        //försök från övningen med hundarna
+        /channelLiEl.value = channel;
+
+        //add text(value)
+        channelLiEl.textContent = channel;
+
+        channel
+
+        channelsEl.appendChild(channelLiEl);/
+
+
+        //BEHÖVER FÅ UT KANALNAMN SÅ KEYS I GETCHANNELS ÄR KANSKE FEL??
+*/
