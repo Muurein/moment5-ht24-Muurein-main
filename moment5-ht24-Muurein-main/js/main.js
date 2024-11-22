bbb9b59c9bf2aed1651c871d452c5f7f62ded6bb
Muurein <amanda.l.thim@gmail.com>
2024-11-22 14:07:10 +0100

    all functions in place

--- moment5-ht24-Muurein-main/js/main.js
+++ moment5-ht24-Muurein-main/js/main.js
@@ -32,11 +32,11 @@ document.getElementById("shownumrows").style.display = "none"; // Radera denna r
 window.onload = init;
 
 
+
+
 //1a - gets and shows a list of the channels when the page loads 
 function init() {
     getChannels();
-   // showInfo();
-    getTVTable();
 }
 
 //1aa + 1ab - get channels to be able to show them in the list
@@ -49,10 +49,9 @@ function getChannels() {
         //turn the response into JSON
         .then(response => response.json())
         //gets the data
-        .then(data => { 
-            console.log(data)
-            writeChannels(data.channels)})
+        .then(data => {writeChannels(data.channels)})
         .catch(error => console.log("There was an error " + error))
+
     }
 
 //1ac + 1ad + 1b
@@ -60,32 +59,35 @@ function writeChannels(channels) {
 
     //get the HTML-element where I want to write out the channels
     const channelsEl = document.getElementById("mainnavlist");
-   // console.log(channels);
 
     //loops through the given array to create the list of channels
     channels.forEach(channel => {
-
          //creates the list item and adds the "hovering-box"
         let channelLiEl = document.createElement("li");
         let listTextEl = document.createTextNode(channel.name);
         channelLiEl.setAttribute("title", channel.tagline);
-        channelLiEl.value = channel;
+        
+        channelLiEl.addEventListener("click", () => {getTVTable(channel.id)});
 
 
         //smush them together
         channelLiEl.appendChild(listTextEl);
         channelsEl.appendChild(channelLiEl);
         
+       
     })
-    
+    //channelsEl.addEventListener("click", getTVTable); //förut var det getTableP1
 }
 
 
-//pre-2 - get the channel programs
-function getTVTable() {
-    let url = "http://api.sr.se/api/v2/scheduledepisodes/rightnow?format=json"; 
 
-    console.log(url);
+
+
+//pre-2 - get the channel programs
+function getTVTable(channelId) {
+    const url = `http://api.sr.se/api/v2/scheduledepisodes?channelid=${channelId}&format=json&pagination=false`; 
+    console.log(channelId);
+    clearTVTable();
 
     //fetch - calls for the url
     fetch(url)
@@ -93,57 +95,82 @@ function getTVTable() {
         .then(response => response.json())
         //från Malins övning - vill få ut värdet ur nycklarna, dock var det variabler så det kanske inte funkar - VALUES ÄR FEL
         //.then(data => writeChannels(Object.keys(data.channels))) //KAN OCKSÅ TESTA BARA SKRIVA .then(data => writeChannels.data), får inga siffror bara [object object] .then(data => console.log(data.channels))
-        .then(data => writeTVTable(data.channels))
-        .catch(error => console.log("There was an error " + error))
+        .then(data => {writeTVTable(data.schedule)})
+        //.catch(error => console.log("There was a schedule error " + error))
 }
 
 
 
 
 //2aa + 2ab + 2ac + 2ad write out the channel programs
-function writeTVTable() {
-    //console.table(channels.name.title.description);
+function writeTVTable(schedule) {
+    //get div-element where the table will be written out
     let tableEl = document.getElementById("info");
 
-    //console.log(tableEl);
-
-    //skapa datum??
+    //loops through and adds elements <article><h2>title</h2><h4>subtitle</h4><p>description</p></article>
+    schedule.forEach(episode => {
+        //convert endtimeutc to JavaScript-date to be able to handle it
+        let episodeEndDateTime = episode.endtimeutc.replace(/\D/g, ''); //replaces everything that isn't numbers in string
+        episodeEndDateTime = new Date(parseInt(episodeEndDateTime)); //converts unix-timestamp-string to numbers and gives timestamp and it converts to hours, minuites etc.
+        
+        //if episode has ended, stop this loop-iteration
+        if(episodeEndDateTime < Date.now()) {
+            return;
+        }
     
+        
+        let articleEl = document.createElement("article");
 
+        //title h2
+        let heading2El = document.createElement("h2");
+        let heading2TextEl = document.createTextNode(episode.title);
+        heading2El.appendChild(heading2TextEl);
+
+        //subtitle h4
+        let heading4El = document.createElement("h4");
+        //adds empty string if there is no subtitle
+        let heading4TextEl = document.createTextNode(episode.subtitle || "");
+        heading4El.appendChild(heading4TextEl);
+
+        //description
+        let descEl = document.createElement("p");
+        let descTextEl = document.createTextNode(episode.description);
+        descEl.appendChild(descTextEl)
+
+        //time
+        let newDivEl = document.createElement("div");
+        let dateText = document.createTextNode(getDateAndTime(episode.starttimeutc)+ " - " +getDateAndTime(episode.endtimeutc));
+        newDivEl.appendChild(dateText);
+
+
+        //smush them all together
+        articleEl.appendChild(heading2El);
+        articleEl.appendChild(heading4El);
+        articleEl.appendChild(descEl);
+        articleEl.appendChild(newDivEl);
+        tableEl.appendChild(articleEl);
+        
+    })
 }
 
+function getDateAndTime(dateStr) {
+    //convert endtimeutc to JavaScript-date to be able to handle it
+    let onlyNumbers = dateStr.replace(/\D/g, ''); //replaces everything that isn't numbers in string
+    onlyNumbers = parseInt(onlyNumbers); //converts unix-timestamp-string to numbers
+
+    const date = new Date(onlyNumbers); //gives timestamp and it converts to hours, minuites etc.
+    let time = date.toLocaleString("sv-SE", {hour:"numeric", minute:"numeric"});
 
 
+     return time;
+}
 
-/*
-        //creates the list item
-       // let channelLiEl = document.createElement("li");
+//remove from list when clicking on another channel
+function clearTVTable() {
+    let tableEl = document.getElementById("info");
 
+    while(tableEl.firstChild) {
+        tableEl.removeChild(tableEl.firstChild);
+    }
 
-
-
-       /försök från föreläsning
-       //add text(value)
-       let textNode = document.createTextNode(channels.name); //BLIR BARA UNDEFINED VA FAN DÅ FÖR
-       channelLiEl.appendChild(textNode);
-       channelsEl.appendChild(channelLiEl);/
-
-       //eget försök som inte funkade
-        itemEl.classname = "item";
-
-        document.getElementsByClassName("item").innerHTML = channel.name;
-        console.log(channel.name);/
-
-        //försök från övningen med hundarna
-        /channelLiEl.value = channel;
-
-        //add text(value)
-        channelLiEl.textContent = channel;
-
-        channel
-
-        channelsEl.appendChild(channelLiEl);/
-
-
-        //BEHÖVER FÅ UT KANALNAMN SÅ KEYS I GETCHANNELS ÄR KANSKE FEL??
-*/
+}
